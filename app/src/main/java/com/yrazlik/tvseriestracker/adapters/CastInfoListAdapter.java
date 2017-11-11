bd2b99eb174b57e69de7df94d285a1752d7e85a2
Yasin Razlık <yrazlik@Yasins-MacBook-Pro.local>
2017-11-11 16:23:41 +0300

    Cast fragment ok

---
+++ app/src/main/java/com/yrazlik/tvseriestracker/adapters/CastInfoListAdapter.java
@@ -0,0 +1,71 @@
+package com.yrazlik.tvseriestracker.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.yrazlik.tvseriestracker.R;
+import com.yrazlik.tvseriestracker.data.CastDto;
+import com.yrazlik.tvseriestracker.data.CharacterDto;
+import com.yrazlik.tvseriestracker.data.ImageDto;
+import com.yrazlik.tvseriestracker.data.PersonDto;
+import com.yrazlik.tvseriestracker.util.PicassoImageLoader;
+import com.yrazlik.tvseriestracker.view.RobotoTextView;
+
+import java.util.List;
+
+/**
+ * Created by yrazlik on 11.11.2017.
+ */
+
+public class CastInfoListAdapter extends ArrayAdapter<CastDto> {
+
+    private Context mContext;
+    private List<CastDto> cast;
+
+    public CastInfoListAdapter(Context context, int resource, List<CastDto> cast) {
+        super(context, resource, cast);
+        this.mContext = context;
+        this.cast = cast;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            convertView = inflater.inflate(R.layout.list_row_cast, parent, false);
+
+            holder = new ViewHolder();
+            holder.castIV = convertView.findViewById(R.id.castIV);
+            holder.personTV = convertView.findViewById(R.id.personTV);
+            holder.characterTV = convertView.findViewById(R.id.characterTV);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        CastDto cast = getItem(position);
+        PersonDto personDto = cast.getPerson();
+        CharacterDto characterDto = cast.getCharacter();
+        ImageDto castImg = cast.getPerson() == null ? null : cast.getPerson().getImage();
+
+        PicassoImageLoader.getInstance(mContext).loadImage(castImg != null ? castImg.getMedium() : "", holder.castIV);
+        holder.personTV.setText(personDto != null ? personDto.getName() : "-");
+        holder.characterTV.setText(characterDto != null ? "as " + characterDto.getName() : "-");
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        public ImageView castIV;
+        public RobotoTextView personTV;
+        public RobotoTextView characterTV;
+    }
+}
--- app/src/main/java/com/yrazlik/tvseriestracker/fragments/ShowCastInfoFragment.java
+++ app/src/main/java/com/yrazlik/tvseriestracker/fragments/ShowCastInfoFragment.java
@@ -2,11 +2,13 @@ package com.yrazlik.tvseriestracker.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.ListViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.yrazlik.tvseriestracker.R;
+import com.yrazlik.tvseriestracker.adapters.CastInfoListAdapter;
 import com.yrazlik.tvseriestracker.data.ShowDto;
 
 /**
@@ -14,6 +16,9 @@ import com.yrazlik.tvseriestracker.data.ShowDto;
  */
 
 public class ShowCastInfoFragment extends BaseFragment{
+
+    private ListViewCompat castLV;
+    private CastInfoListAdapter castInfoListAdapter;
 
     private ShowDto showDto;
 
@@ -27,7 +32,21 @@ public class ShowCastInfoFragment extends BaseFragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
+        initUI();
         return rootView;
+    }
+
+    private void initUI() {
+        castLV = rootView.findViewById(R.id.castLV);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(showDto.get_embedded() != null && showDto.get_embedded().getCast() != null) {
+            castInfoListAdapter = new CastInfoListAdapter(getContext(), R.layout.list_row_cast, showDto.get_embedded().getCast());
+            castLV.setAdapter(castInfoListAdapter);
+        }
     }
 
     @Override
@@ -41,6 +60,6 @@ public class ShowCastInfoFragment extends BaseFragment{
     }
 
     public int getTitle() {
-        return R.string.fragment_title_show_summary_info;
+        return R.string.fragment_title_show_cast_info;
     }
 }
