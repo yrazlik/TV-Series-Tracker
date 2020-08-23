64e787ee48d703178cd39184fdc1b45d7a5c43c8
Yasin Razlik <yrazlik@gmail.com>
2020-08-23 11:25:05 +0300

    ads impl

--- app/src/main/java/com/yrazlik/tvseriestracker/TvSeriesTrackerApp.java
+++ app/src/main/java/com/yrazlik/tvseriestracker/TvSeriesTrackerApp.java
@@ -3,10 +3,14 @@ package com.yrazlik.tvseriestracker;
 import android.app.Application;
 import android.content.Context;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
 import com.yrazlik.tvseriestracker.data.EpisodeDto;
 import com.yrazlik.tvseriestracker.data.ShowDto;
 import com.yrazlik.tvseriestracker.util.Utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -25,6 +29,19 @@ public class TvSeriesTrackerApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        List<String> testDevices = new ArrayList<>();
+        testDevices.add("9D9D289B3C0209123947B9B256CD2AB0");
+        RequestConfiguration requestConfiguration
+                = new RequestConfiguration.Builder()
+                .setTestDeviceIds(testDevices)
+                .build();
+
+        // val deviceIds = arrayListOf(AdRequest.DEVICE_ID_EMULATOR)
+        // getDeviceIdForAdMobTestAds(context)?.let { deviceIds.add(it.toUpperCase(Locale.ROOT)) }
+        // MobileAds.setRequestConfiguration(RequestConfiguration.Builder().setTestDeviceIds(deviceIds).build())
+
+        MobileAds.setRequestConfiguration(requestConfiguration);
+        MobileAds.initialize(getApplicationContext());
         mAppContext = getApplicationContext();
         watchedList = Utils.getWatchedList(mAppContext);
         favoritesList = Utils.getFavoritesList(mAppContext);
